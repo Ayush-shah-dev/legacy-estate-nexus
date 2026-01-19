eacc23ff87f9c725432ad33e43e3043d93548e75
Priyanshu Uchat <priyanshu.uc507@gmail.com>
2026-01-19 13:58:19 +0530

    Some_ignore_Changes

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ echo "Switching to branch master" git checkout master
 echo "Building app..."
 npm run build
 echo "Deploying files to serevr..."
-scp -r dist/ priyanshu@31.97.227.221:/var/www/buddingtech.in/
+scp -r dist/ priyanshu@31.97.227.221:/var/www/regalestateconsultants.com//
 echo "Done!"
